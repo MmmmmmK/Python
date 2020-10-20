f9c0e6bf881c77248871bf8065c9f66e5a35a23a
MmmmmmK <kramerfaggot@mail.ru>
2020-10-20 21:27:26 +0600

    Nasty_Version

---
+++ main.py
@@ -0,0 +1,15 @@
+# class Exception(Exception):
+#     def __init__(self):
+from typing import List, Union
+
+data: List[str] = ['Tom', 'Peter', 'Vasyl', 'Kola']
+
+def writeTable(valuesOfTable: List) -> None:
+    """Вырисовывание таблицы"""
+    print('_____________')
+    for sequence, names in enumerate(valuesOfTable):
+        print('|{:> 4d}|{:>7s}|'.format(sequence, names))
+    print('______________')
+
+
+writeTable(data)
