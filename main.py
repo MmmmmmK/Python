1875d133c1c4b74afbbbd21a10c8407d75c08d72
MmmmmmK <67944399+MmmmmmK@users.noreply.github.com>
2020-10-21 22:28:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@
 from typing import List, Union
 
 data: List[str] = ['Tom', 'Peter', 'Vasyl', 'Kola']
+line = '_' * 14
 
-def writeTable(valuesOfTable: List) -> None:
+def write_table(columns: Union[List[Any], Tuple[Any]], rows:Union[List[Union[List[Any], Tuple[Any]]], Tuple[Union[List[Any], Tuple[Any]]]]) -> None:
     """Вырисовывание таблицы"""
-    print('_____________')
+    print(line)
     for sequence, names in enumerate(valuesOfTable):
         print('|{:> 4d}|{:>7s}|'.format(sequence, names))
-    print('______________')
+    print(line)
 
 writeTable(data)
