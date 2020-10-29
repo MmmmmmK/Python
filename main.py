76b260fac1c57248897aaedec75c650aa5b50ea5
MmmmmmK <67944399+MmmmmmK@users.noreply.github.com>
2020-10-30 01:09:11 +0600

    In progress
    Направляю программу в нужное русло =)

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from typing import List, Union, Any, Tuple
 
-data: List[str] = ['Tom', 'Peter', 'Vasyl', 'Kola']
 line = '_' * 14
-rows = int(input('Введите количество элементов: '))
+rows = []
+columns: Tuple[str] = tuple(input('Введите элементы-заголовки: ').split())
+rows : List[Tuple[Any]] = tuple(input('Введите элементы-данные строк: ').split())
 
 
-def write_table(rows : Union[List[Union[List[Any], Tuple[Any]]], Tuple[Union[List[Any], Tuple[Any]]]], valuesOfTable : str) -> str:
+def write_table(columns: Union[List[Any], Tuple[Any]], rows : Union[List[Union[List[Any], Tuple[Any]]], Tuple[Union[List[Any], Tuple[Any]]]], valuesOfTable : str) -> str:
     """Вырисовывание таблицы"""
-    answer = ' '
     for sequence in range(rows):
         answer += ('|{:> 4d}|{:>7s}|\n'.format(sequence + 1, valuesOfTable))
     return line + answer + line
 
 
-print(writeTable(rows, data))
+print(writeTable(columns, rows))
