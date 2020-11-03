853ef846b1a3d4dd47e484de0a9463cc19a90504
MmmmmmK <67944399+MmmmmmK@users.noreply.github.com>
2020-11-04 00:33:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from typing import List, Union, Any, Tuple
 
-line = '_' * 14
-rows = []
-columns: Tuple[str] = tuple(input('Введите элементы-заголовки: ').split())
-rows : List[Tuple[Any]] = tuple(input('Введите элементы-данные строк: ').split())
+line = '_' * 14 + '\n'
+columns: List[str] = []
+rows : List[Tuple[Any]] = [(1, 'Peter', 'Johnson', 'Alexandrovich', '17.09.2000'), (2, 'Mark', 'Colinson', 'Petrovich', '17.09.2001')]
 
 
-def write_table(columns: Union[List[Any], Tuple[Any]], rows : Union[List[Union[List[Any], Tuple[Any]]], Tuple[Union[List[Any], Tuple[Any]]]], valuesOfTable : str) -> str:
+def writeTable(columns: Union[List[Any], Tuple[Any]], rows : Union[List[Union[List[Any], Tuple[Any]]], Tuple[Union[List[Any], Tuple[Any]]]]) -> str:
     """Вырисовывание таблицы"""
-    for sequence in range(rows):
-        answer += ('|{:> 4d}|{:>7s}|\n'.format(sequence + 1, valuesOfTable))
-    return line + answer + line
+    drawRaw1 = ('|{columns[0]!s:>4}|{columns[1]!s:>15}|{columns[2]!s:>15}|{columns[3]!s:>15}|{columns[4]!s:>15}'.format(columns=['ID', 'Name', 'Last', 'Patronymic', 'BD']))
+    answer = ''
+    for sequence, value in enumerate(rows):
+        answer += '|{value[sequence][0]:>4d}|{value[sequence][1]:>15s}|{value[sequence][2]:>15s}|{value[sequence][3]:>15s}|{value[sequence][4]:>15s}|\n'.format(value)
+    return line + drawRaw1 + answer + line
 
 
 print(writeTable(columns, rows))
