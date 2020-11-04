43e6f9f12178e8bf32e0d28ddb295a980b334f27
MmmmmmK <67944399+MmmmmmK@users.noreply.github.com>
2020-11-04 21:15:24 +0600

    Edit
    - Не работает в format (строка 12). Можно заставить работать?
    - Ты в первом своем review сказал, что пользователь вводит в raws и columns, я пишу сам...

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from typing import List, Union, Any, Tuple
 
-line = '_' * 14 + '\n'
-columns: List[str] = []
+line = '_' * 64 + '\n'
 rows : List[Tuple[Any]] = [(1, 'Peter', 'Johnson', 'Alexandrovich', '17.09.2000'), (2, 'Mark', 'Colinson', 'Petrovich', '17.09.2001')]
+columns = ['ID', 'Name', 'Last', 'Patronymic', 'BD']
 
-
-def writeTable(columns: Union[List[Any], Tuple[Any]], rows : Union[List[Union[List[Any], Tuple[Any]]], Tuple[Union[List[Any], Tuple[Any]]]]) -> str:
+def writeTable(columns : Union[List[Any], Tuple[Any]], rows : Union[List[Union[List[Any], Tuple[Any]]], Tuple[Union[List[Any], Tuple[Any]]]]) -> str:
     """Вырисовывание таблицы"""
-    drawRaw1 = ('|{columns[0]!s:>4}|{columns[1]!s:>15}|{columns[2]!s:>15}|{columns[3]!s:>15}|{columns[4]!s:>15}'.format(columns=['ID', 'Name', 'Last', 'Patronymic', 'BD']))
+    drawRaw1 = '|{0[0]!s:>4}|{0[1]!s:>15}|{0[2]!s:>15}|{0[3]!s:>15}|{0[4]!s:>15}|\n'.format(columns)
     answer = ''
     for sequence, value in enumerate(rows):
-        answer += '|{value[sequence][0]:>4d}|{value[sequence][1]:>15s}|{value[sequence][2]:>15s}|{value[sequence][3]:>15s}|{value[sequence][4]:>15s}|\n'.format(value)
+        answer += '|{1[{0}][0]:>4d}|{1[{0}][1]:>15s}|{1[{0}][2]:>15s}|{1[{0}][3]:>15s}|{1[{0}][4]:>15s}|\n'.format(sequence, value)
     return line + drawRaw1 + answer + line
 
 
