ee51e430c2f0724f539739f9b7cee182a970d947
MmmmmmK <67944399+MmmmmmK@users.noreply.github.com>
2020-11-08 22:55:36 +0600

    The final is close
    - добавил обработку ошибок, которая не работает, т.к. не распознает 2-ой аргумент ( raise inputException(len(columns), len(row)))
    - в format только строки 
    - определена длина верхней границы, но в строке 34 lambda не рассматривается как число 
    - format(*value) не работает

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from typing import List, Union, Any, Tuple
 
-line = '_' * 64 + '\n'
+def inputException(Exception):
+    '''Класс обработки количества введенных данных'''
+    def __init__(self, waiting, fact):
+        Exception.__init__(self)
+        self.waiting = waiting
+        self.fact = fact
+
 rows : List[Tuple[Any]] = [(1, 'Peter', 'Johnson', 'Alexandrovich', '17.09.2000'), (2, 'Mark', 'Colinson', 'Petrovich', '17.09.2001')]
 columns = ['ID', 'Name', 'Last', 'Patronymic', 'BD']
 
+
 def writeTable(columns : Union[List[Any], Tuple[Any]], rows : Union[List[Union[List[Any], Tuple[Any]]], Tuple[Union[List[Any], Tuple[Any]]]]) -> str:
     """Вырисовывание таблицы"""
-    drawRaw1 = '|{0[0]!s:>4}|{0[1]!s:>15}|{0[2]!s:>15}|{0[3]!s:>15}|{0[4]!s:>15}|\n'.format(columns)
     answer = ''
-    for sequence, value in enumerate(rows):
-        answer += '|{1[{0}][0]:>4d}|{1[{0}][1]:>15s}|{1[{0}][2]:>15s}|{1[{0}][3]:>15s}|{1[{0}][4]:>15s}|\n'.format(sequence, value)
+    rows_as_str = []
+    lens = list(map(len, columns))
+    for row in rows:
+        rows_as_str.append(tuple(map(str, row)))
+        try:
+            if len(columns) != len(row):
+                raise inputException(len(columns), len(row))
+        except inputException as ex:
+            print('Вы ввели {0}, а нужно {1}! Введите имя, фамилию, отчество, год рождения:'.format(ex.fact, ex.waiting), end=' ')
+            rows[rows.index(row)] = input().split()
+            rows[rows.index(row)].insert(0, rows.index(row) + 1)  # чтоб пользователь не писал лишнее
+        for num, data in enumerate(row):
+            if len(str(data)) > lens[num]:
+                lens[num] = len(str(data))
+    drawRaw1 = '|{0[0]!s:>{1[0]}}|{0[1]!s:>{1[1]}}|{0[2]!s:>{1[2]}}|{0[3]!s:>{1[3]}}|{0[4]!s:>{1[4]}}|\n'.format(columns, lens)
+    for value in rows_as_str:
+        answer += '|{0}|{1}|{2}|{3}|{4}|\n'.format(*value)
+    line = '_' + (lambda lens: lens[0] + lens[1] + lens[2] + lens[3] + lens[4] + 6) + '\n'
     return line + drawRaw1 + answer + line
 
 
